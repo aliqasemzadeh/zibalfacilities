42e5a4563a92b2bec483bdc87ed076072bcbea85
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-11-03 08:14:39 +0330

    Add initial project structure and Composer setup
    Created `composer.json` with necessary package information and dependencies. Updated `.gitignore` to exclude `composer.lock` and `/vendor/` directory. Added a basic `ServiceProvider.php` class extending `Illuminate\Support\ServiceProvider`.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
