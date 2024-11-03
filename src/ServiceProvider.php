36442fe9911808351716ef8ed5bfa35790f47d46
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-11-03 08:38:48 +0330

    Add Zibal config file and update service provider
    Created a new configuration file for Zibal and included it in the publishable resources. Updated the PSR-4 autoload namespace in composer.json and added Laravel service provider registration.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot(): void
     {
-
+        $this->publishes([
+            __DIR__.'/../config/zibal.php' => config_path('zibal.php'),
+        ]);
     }
 
     public function register(): void
