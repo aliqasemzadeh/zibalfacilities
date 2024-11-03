fd1e7c392b01eb2023738c054096caf2eaf2a65b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-11-03 11:56:02 +0330

    Refactor Facility class and fix Zibal API URL.
    Updated `Facility` class to use protected properties and Guzzle's `sendAsync` method for API calls. Fixed configuration file to remove trailing slash from Zibal API URL.

--- config/zibal.php
+++ config/zibal.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'api_token' => env('ZIBAL_API_TOKEN'),
-    'api_url' => env('ZIBAL_API_URL', 'https://api.zibal.ir/v1/facility/')
+    'api_url' => env('ZIBAL_API_URL', 'https://api.zibal.ir/v1/facility')
 ];
--- src/Facility.php
+++ src/Facility.php
@@ -1,11 +1,13 @@
 <?php
 namespace AliQasemzadeh\ZibalFacilities;
 
-class Facility {
-    private $token;
-    public $api_url;
+use GuzzleHttp\Psr7\Request;
 
-    public function __construct(string $token = null, ?string $api_url = null)
+class Facility {
+    protected $token;
+    protected $api_url;
+
+    public function __construct(?string $token = null, ?string $api_url = null)
     {
         if($token) {
             $this->setToken($token);
@@ -16,26 +18,9 @@ class Facility {
         if($api_url) {
             $this->setApiUrl($api_url);
         } else {
-            $this->setToken(config('zibal.api_url'));
+            $this->setApiUrl(config('zibal.api_url'));
         }
 
-    }
-
-    public function call(string $name, array $params)
-    {
-        $client = new \GuzzleHttp\Client();
-        try {
-            $response = $client->request('POST', $this->api_url . '/' . $name, [
-                'json' => $params,
-                'headers' => [
-                    'Authorization' => 'Bearer ' . $this->token,
-                    'Content-Type' => 'application/json',
-                ],
-            ]);
-            return $response->getBody()->getContents();
-        } catch (\GuzzleHttp\Exception\ClientException $e) {
-
-        }
     }
 
     public function setToken(string $token)
@@ -47,4 +32,22 @@ class Facility {
     {
         $this->api_url = $url;
     }
+
+    public function call(string $method, array $params)
+    {
+        $client = new \GuzzleHttp\Client();
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => $this->token,
+        ];
+        $body = json_encode($params);
+        try {
+            $request = new Request('POST', $this->api_url . "/" . $method, $headers, $body);
+            $res = $client->sendAsync($request)->wait();
+            return $res->getBody();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+
+        }
+    }
+
 }
