1156f657bbf825e16c5f9c3828654a98f73720cd
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-11-03 10:19:45 +0330

    Add constructor to Facility class
    Introduced a constructor in the Facility class to initialize the token property. The constructor sets the token either from the provided parameter or from the default configuration if no parameter is given.

--- src/Facility.php
+++ src/Facility.php
@@ -4,6 +4,16 @@ class Facility {
     private $token;
     public $api_url;
 
+    public function __construct(?string $token)
+    {
+        if($token) {
+            $this->setToken($token);
+        } else {
+            $this->setToken(config('zibal.api_token'));
+        }
+
+    }
+
     public function call(string $name, array $params)
     {
         $client = new \GuzzleHttp\Client();
